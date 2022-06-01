99a7abecf2719e841fd8148a3d37a90b59edebea
Deep Singhvi <dsinghvi@umich.edu>
2022-06-01 11:45:51 -0700

    setup repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# gradle-spring-starter
+Template repo containing a java gradle project that runs a Spring Server.
---
+++ src/main/java/com/JerseyConfig.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+    }
+}
